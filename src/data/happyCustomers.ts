0d690dc5105db0ba234de8e640ec152b05a73aad
sidwokenup_ <wakedupsid2023@gmail.com>
2026-03-10 21:33:29 +0530

    jpg

--- src/data/happyCustomers.ts
+++ src/data/happyCustomers.ts
@@ -14,6 +14,6 @@ export const HAPPY_CUSTOMERS: CustomerTestimonial[] = [
     location: "Gurugram",
     occasion: "Mehndi Ceremony",
     review: "During my wedding rush, Style By Divya designed the perfect dress for my Mehndi ceremony exactly how I imagined it",
-    image: "/assets/customers/chetna.jpg",
+    image: "/assets/customers/chetna.JPG",
   },
 ];
